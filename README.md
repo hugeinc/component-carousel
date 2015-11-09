dbb305173c64de1a0774743d9ab8dc4a21e73da0
wes hatch <wes.hatch@gmail.com>
2015-11-09 00:42:51 -0500

    moving flexicarousel-3 files over

--- README.md
+++ README.md
@@ -1,40 +1,81 @@
 # Flexicarousel
 
-A carousel that'll dynamically adapt its width: it will grow or shrink alongside a site that’s resizing via css media queries. Uses CSS3 transforms for its transitions
+A carousel that'll dynamically adapt its width: it will grow or shrink alongside a site that’s resizing via css media queries. Uses CSS3
+transforms for its transitions, and is also responsive using CSS.
 
-Originally Inspired by:
-responsive-carousel
-https://github.com/filamentgroup/responsive-carousel
-(c) 2012 Filament Group, Inc.
+##Introduction
+
+The general idea is that this component should maintain a separation of state and style. That is to say, the Javascript should maintain
+the state of the carousel (what slide, etc), while the CSS should take care of the presentation of this state (ie. transitions between
+slides, responsive, etc).
+
+##Overview
+
+Featuring a touch-based interface, simple API, and a very-lightweight footprint. It does the basics well, but that's it. No bloat.
+You can swipe to drag a slide yet still use CSS to control how the slide transitions will behave. You can also choose to change slides by
+using the exposed API. The carousel works on both desktop and mobile, using only CSS to control the look and feel. Under 2 KB(!)
 
 ## Getting Started
-Download the [production version][min] or the [development version][max].
+Download the [production version][min] or the [development version][max]. Or the [jquery version][jquery].
 
-[min]: https://raw.github.com/apathetic/flexicarousel/master/dist/flexicarousel.min.js
-[max]: https://raw.github.com/apathetic/flexicarousel/master/dist/flexicarousel.js
+[min]: https://github.com/apathetic/flexicarousel-3/blob/master/dist/flexicarousel.min.js
+[max]: https://github.com/apathetic/flexicarousel-3/blob/master/dist/flexicarousel.js
+[jquery]: https://github.com/apathetic/flexicarousel-3/blob/master/dist/jquery.flexicarousel.min.js
 
-In your web page:
+Include the relevant scripts in your web page, and then:
 
 ```html
-<script src="jquery.js"></script>
-<script src="flexicarousel.min.js"></script>
 <script>
-jQuery(function($) {
-	$('.carousel').carousel({
-		// options
-	});
-});
+
+	// availble options
+	var options = {
+		activeClass: 'active',
+		slideWrap: 'ul',			// for binding touch events
+		slides: 'li',				// the slide
+		infinite: true,				// infinite scrolling or not
+		display: 1,					// if infinite, the # of slides to "view ahead" ie. position offscreen
+		disableDragging: false
+	};
+
+	var container = document.querySelector('.carousel');
+	var carousel = new Carousel(container, options);
+
 </script>
 ```
 
-## Documentation
-_(Coming soon)_
+### Notes
+
+The carousel uses transitionEnd (or the browser-prefixed variant) to determine when to update after sliding. If the carousel seems to not be working
+-- and you've messed with the default CSS -- double-check that you're using the _transition_ property correctly in the .animate class.
+
+## API
+
+	next: advances the carousel by one slide
+
+	prev: returns to the previous slide
+
+	go: function(to) advances slide to the _to_ index
+
+
+## Support
+* IE8+
+* Safari / Chrome
+* Firefox
+* iOS
+* Android
 
 ## Known Issues
 
 ## Examples
-_(Coming soon)_
+
+Please see the _test / demo_ directory
 
 ## Release History
-### 0.1.1
-Updated CSS so that only animating relevant properties. Gives much needed performance gain and no stutter-y z-index issues.
+
+### 0.2
+* bug fixes, mostly
+* updated slide engine
+* more robust dragging on mobile
+
+### 0.1
+* first release
--- src/flexicarousel.js
+++ src/flexicarousel.js
@@ -1,250 +1,387 @@
 /*
- * flexicarousel
- * https://github.com/apathetic/flexicarousel
+ * carousel ten billion
+ * https://github.com/apathetic/flexicarousel-3
  *
  * Copyright (c) 2013 Wes Hatch
  * Licensed under the MIT license.
  *
  */
 
-/*jslint debug: true, evil: false, devel: true*/
 
+var Carousel = function(container, options){
 
-// TODO make jquery agnostic
+	this.handle = container;
 
-(function($) {
+	// default options
+	// --------------------
+	this.defaults = {
+		activeClass: 'active',
+		slideWrap: 'ul',			// for binding touch events
+		slides: 'li',				// the slide
+		infinite: true,				// infinite scrolling or not
+		display: 1,					// the minimum # of slides to display at a time
+		offscreen: 0,				// the # of slides to "view ahead" ie. position offscreen
+		disableDragging: false
+	};
 
+	// state vars
+	// --------------------
+	this.current = 0;
+	this.slides = [];
+	this.sliding = false;
+	this.cloned = 0;
 
+	// touch vars
+	// --------------------
+	this.dragging = false;
+	this.dragThreshold = 50;
+	this.deltaX = 0;
 
-  // var browser = {
-  //   addEventListener: !!window.addEventListener,
-  //   touch: ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
-  //   transitions: (function(temp) {
-  //     var props = ['transitionProperty', 'WebkitTransition', 'MozTransition', 'OTransition', 'msTransition'];
-  //     for ( var i in props ) if (temp.style[ props[i] ] !== undefined) return true;
-  //     return false;
-  //   })(document.createElement('swipe'))
-  // };
+	// feature detection
+	// --------------------
+	this.isTouch = 'ontouchend' in document;
+	var style = document.body.style;
+	var tests = {
+		 'transform':'transitionend',
+		 'OTransform': 'oTransitionEnd',
+		 'MozTransform': 'transitionend',
+		 'webkitTransform': 'webkitTransitionEnd'
+	};
+	// note: we don't test "ms" prefix, (as that gives us IE9 which doesn't support transforms3d anyway. IE10 test will work with "transform")
+	for (var x in tests) {
+		if ( style[x] !== undefined) {
+			this.transform = x;
+			this.transitionEnd = tests[x];
+			break;
+		}
+	}
 
+	// engage engines
+	// --------------------
+	this.init(options);
+};
 
+Carousel.prototype = {
 
+	/**
+	 * Initialize the carousel and set some defaults
+	 * @param  {object} options List of key: value options
+	 * @return {void}
+	 */
+	init: function(options){
 
+		// set up options
+		this.options = this._extend( this.defaults, options );
 
-	// from: http://www.modernizr.com
-	var transitions = (function(){
-		var transitionEnd = (function(){
-			var t,
-				el = document.createElement('fake'),
-				transitions = {
-				'transition': 'transitionend',
-				'OTransition': 'oTransitionEnd otransitionend',
-				'MozTransition': 'transitionend',
-				'WebkitTransition': 'webkitTransitionEnd'
-			};
+		// find carousel elements
+		if ( !(this.slideWrap	= this.handle.querySelector(this.options.slideWrap)) ) { return; }		// note: assignment
+		if ( !(this.slides		= this.slideWrap.querySelectorAll(this.options.slides)) ) { return; }	// note: assignment
 
-			for(t in transitions){
-				if( el.style[t] !== undefined ){
-					return transitions[t];
-				}
-			}
-		}());
+		this.numSlides = this.slides.length;
 
-		return transitionEnd && {end: transitionEnd}
-	})();
+		// check if we have sufficient slides to make a carousel
+		if ( this.numSlides < this.options.display ) { this.sliding = true; return; }		// this.sliding deactivates carousel. I will better-ify this one day. Maybe "this.active" ?
+		if ( this.options.infinite ) { this._cloneSlides(this.options.display); }
 
+		this.go(0);
 
-	var current = 0,
-			slides,
-			sliding = false,
-			inClass, outClass, activeClass, beforeClass, afterClass,
-			registrationCallbacks = [];
-
-
-	var methods = {
-		init: function( opts ){
-
-			var trans;
-					optsAttr = $(this).attr('data-options');
-			// var data;
-
-			eval('var data='+optsAttr);
-			this.options = $.extend( {}, $.fn.carousel.defaults, data, opts );
-
-			trans = this.options.transition;
-			slides = $(this).find(this.options.slide);
-			if (!slides.length) { console.log('flexicarousel: no slides found'); return; }
-
-			// put these into closure to simplify our lives
-			inClass = this.options.inClass;
-			outClass = this.options.outClass;
-			activeClass = this.options.activeClass;
-			beforeClass = this.options.beforeClass;
-			afterClass = this.options.afterClass;
-
-			slides.eq(0).addClass( activeClass );
-
-			// var trans = trans.split(',');		// TODO make multiple transitions possible
-			if( !trans ){ transitions = false; }
-
-			$(this).addClass( 'carousel ' + ( trans ? 'carousel-' + trans : '' ) );
-			$(this).trigger( 'init.carousel', this );		// useful for binding additional functionality
-
-			for (var i = 0; i < registrationCallbacks.length; i++) {
-				registrationCallbacks[i].call(this);
-			}
-
-			return $(this);
-		},
-
-/*
-		autoRotate: function( rotate ) {
-			if (rotate) {
-				var self = this;
-				this.timer = setInterval(function(){
-					$(self).carousel('next');
-				}, 5000);
+		// set up Events
+		if ( ! this.options.disableDragging) {
+			if ( this.isTouch ) {
+				this.slideWrap.addEventListener('touchstart', this._dragStart.bind(this));
+				this.slideWrap.addEventListener('touchmove', this._drag.bind(this));
+				this.slideWrap.addEventListener('touchend', this._dragEnd.bind(this));
+				this.slideWrap.addEventListener('touchcancel', this._dragEnd.bind(this));
 			} else {
-				clearTimeout(this.timer);
+				this.slideWrap.addEventListener('mousedown', this._dragStart.bind(this));
+				this.slideWrap.addEventListener('mousemove', this._drag.bind(this));
+				this.slideWrap.addEventListener('mouseup', this._dragEnd.bind(this));
+				this.slideWrap.addEventListener('mouseleave', this._dragEnd.bind(this));
 			}
-		},
-
-		pause: function (e) {
-			if (!e) this.paused = true
-			if (this.$element.find('.next, .prev').length && transitions) {
-				this.$element.trigger(transitions.end)
-				this.cycle(true)
-			}
-			clearInterval(this.interval)
-			this.interval = null
-			return this
-		},
-*/
-
-		next: function(){
-			$( this ).carousel( 'go', current + 1);
-		},
-
-		prev: function(){
-			$( this ).carousel( 'go', current - 1);
-		},
-
-		go: function( to ){
-
-			var direction,
-					$from,
-					$to;
-
-
-      // determine direction:  1: backward, -1: forward
-      direction = Math.abs(current - to) / (current - to);
-
-      // check bounds
-      if (this.options.infinite) {
-				to = (slides.length + (to % slides.length)) % slides.length;
-			} else {
-				to = Math.max( Math.min(slides.length-1, to), 0);
-			}
-
-      if (to == current || sliding) { return; }
-
-			$from = slides.eq(current),
-			$to = slides.eq(to);
-
-			$to.addClass( ( direction > 0 ? beforeClass : afterClass ) );
-
-			if( transitions ){
-				$(this).carousel( 'transitionStart', $from, $to );
-			} else {
-				$(this).carousel( 'transitionEnd', $from, $to );
-			}
-
-			$(this).trigger( 'go.carousel', to );
-			current = to;
-		},
-
-		transitionStart: function( $from, $to ){
-			var $self = $(this);
-
-			sliding  = true;
-
-			/*jsl:ignore*/
-			$to[0].offsetHeight;				// force a repaint to position this element. *Important*
-			/*jsl:end*/
-
-			$to.one( transitions.end, function() {
-				$self.carousel( 'transitionEnd', $from, $to );
-			});
-
-			$from.addClass( outClass );
-			$to.addClass( inClass );
-		},
-
-		transitionEnd: function( $from, $to ){
-			$from.removeClass( [ activeClass, outClass ].join( ' ' ) );
-			$to.removeClass( [ beforeClass, afterClass, inClass ].join( ' ' ) );
-			$to.addClass( activeClass );
-			sliding  = false;
-			$(this).trigger('slid');
-		},
-
-		destroy: function(){
-			// TODO
-		},
-
-		register: function( callback ){
-			registrationCallbacks.push(callback);
 		}
 
-	};
+		window.addEventListener('resize', this._updateView.bind(this));
+		window.addEventListener('orientationchange', this._updateView.bind(this));
+
+		return this;
+	},
+
+	/**
+	 * Go to the next slide
+	 * @return {void}
+	 */
+	next: function() {
+		if (this.options.infinite || this.current !== this.numSlides-1) {
+			this.go(this.current + 1);
+		} else {
+			this.go(this.numSlides-1);
+		}
+	},
+
+	/**
+	 * Go to the previous slide
+	 * @return {void}
+	 */
+	prev: function() {
+		if (this.options.infinite || this.current !== 0) {
+			this.go(this.current - 1);
+		} else {
+			this.go(0);		// allow the slide to "snap" back if dragging and not infinite
+		}
+	},
+
+	/**
+	 * Go to a particular slide. Prime the "to" slide by positioning it, and then calling _slide() if needed
+	 * @param  {int} to		the slide to go to
+	 * @return {void}
+	 */
+	go: function(to) {
+		if ( this.sliding ) { return; }
+
+		this.width = this.slides[0].offsetWidth;								// check every time
+		this.offset = this.cloned * this.width;
+
+		if ( to < 0 || to >= this.numSlides ) {									// position the carousel if infinite and at end of bounds
+			var temp = (to < 0) ? this.current+this.numSlides : this.current-this.numSlides;
+			this._slide( -(temp * this.width - this.deltaX ) );
+
+			/* jshint ignore:start */
+			this.slideWrap.offsetHeight;										// force a repaint to actually position "to" slide. *Important*
+			/* jshint ignore:end */
+		}
+
+		to = this._loop(to);
+		this._slide( -(to * this.width), true );
+
+		if (this.options.onSlide) { this.options.onSlide.call(this, to, this.current); }	// note: doesn't check if it's a function
+
+		this._removeClass( this.slides[this.current], this.options.activeClass );
+		this._addClass( this.slides[to], this.options.activeClass );
+		this.current = to;
+	},
 
 
-	$.fn.carousel = function( method ) {
-		var args = arguments,
-				fn = this;
+	// ------------------------------------- "mobile" starts here ------------------------------------- //
 
-		return this.each(function() {
 
-			// check if method exists
-			if (method in $.fn.carousel.prototype) {		// if ( typeof $.fn.carousel.prototype[ method ] == 'function' ) {
-				// console.log( method, args );
-				return $.fn.carousel.prototype[ method ].apply( this, Array.prototype.slice.call( args, 1 ));
+	/**
+	 * Start dragging (via touch)
+	 * @param  {event} e Touch event
+	 * @return {void}
+	 */
+	_dragStart: function(e) {
+		var touches;
+
+		if (this.sliding) {
+			return false;
+		}
+
+		e = e.originalEvent || e;
+		touches = e.touches !== undefined ? e.touches : false;
+
+		this.dragThresholdMet = false;
+		this.dragging = true;
+		this.cancel = false;
+		this.startClientX = touches ? touches[0].pageX : e.clientX;
+		this.startClientY = touches ? touches[0].pageY : e.clientY;
+		this.deltaX = 0;	// reset for the case when user does 0,0 touch
+		this.deltaY = 0;	// reset for the case when user does 0,0 touch
+
+		if (e.target.tagName === 'IMG' || e.target.tagName === 'A') { e.target.draggable = false; }
+	},
+
+	/**
+	 * Update slides positions according to user's touch
+	 * @param  {event} e Touch event
+	 * @return {void}
+	 */
+	_drag: function(e) {
+		var abs = Math.abs,		// helper fn
+			touches;
+
+		if (!this.dragging || this.cancel) {
+			return;
+		}
+
+		e = e.originalEvent || e;
+		touches = e.touches !== undefined ? e.touches : false;
+		this.deltaX = (touches ? touches[0].pageX : e.clientX) - this.startClientX;
+		this.deltaY = (touches ? touches[0].pageY : e.clientY) - this.startClientY;
+
+		// determine if we should do slide, or cancel and let the event pass through to the page
+		if (this.dragThresholdMet || (abs(this.deltaX) > abs(this.deltaY) && abs(this.deltaX) > 10)) {		// 10 from empirical testing
+
+			e.preventDefault();
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+
+			this.dragThresholdMet = true;
+			this._slide( -(this.current * this.width - this.deltaX ) );
+
+		} else if ((abs(this.deltaY) > abs(this.deltaX) && abs(this.deltaY) > 10)) {
+			this.cancel = true;
+		}
+	},
+
+	/**
+	 * Drag end, calculate slides' new positions
+	 * @param  {event} e Touch event
+	 * @return {void}
+	 */
+	_dragEnd: function() {
+		if (!this.dragging || this.cancel) {
+			return;
+		}
+
+		this.dragging = false;
+
+		if ( this.deltaX !== 0 && Math.abs(this.deltaX) < this.dragThreshold ) {
+			this.go(this.current);
+		}
+		else if ( this.deltaX > 0 ) {
+			// var jump = Math.round(this.deltaX / this.width);	// distance-based check to swipe multiple slides
+			// this.go(this.current - jump);
+			this.prev();
+		}
+		else if ( this.deltaX < 0 ) {
+			this.next();
+		}
+
+		this.deltaX = 0;
+	},
+
+
+	// ------------------------------------- carousel engine ------------------------------------- //
+
+
+	/**
+	 * Helper function to translate slide in browser
+	 * @param  {[type]} el     [description]
+	 * @param  {[type]} offset [description]
+	 * @return {[type]}        [description]
+	 */
+	_slide: function(offset, animate) {
+		offset -= this.offset;
+
+		if ( animate ) {
+			this.sliding = true;
+			this._addClass( this.slideWrap, 'animate' );
+
+			var delay = 400;
+			var self = this;
+			setTimeout(function(){
+				self.sliding = false;
+				self._removeClass( self.slideWrap, 'animate' );
+			}, delay);
+
+		}
+
+		if (this.transform) {
+			this.slideWrap.style[this.transform] = 'translate3d(' + offset + 'px, 0, 0)';
+		}
+		else {
+			this.slideWrap.style.left = offset+'px';
+		}
+	},
+
+
+	// ------------------------------------- "helper" functions ------------------------------------- //
+
+
+	/**
+	 * Helper function. Calculate modulo of a slides position
+	 * @param  {int} val Slide's position
+	 * @return {int} the index modulo the # of slides
+	 */
+	_loop: function(val) {
+		return (this.numSlides + (val % this.numSlides)) % this.numSlides;
+	},
+
+	/**
+	 * Update the slides' position on a resize. This is throttled at 300ms
+	 * @return {void}
+	 */
+	_updateView: function() {
+		var self = this;
+		clearTimeout(this.timer);
+		this.timer = setTimeout(function(){ self.go(self.current); }, 300);
+	},
+
+	/**
+	 * Duplicate the first and last N slides so that infinite scrolling can work.
+	 * Depends on how many slides are visible at a time, and any outlying slides as well
+	 * @return {void}
+	 */
+	_cloneSlides: function() {
+		var beg, end,
+			duplicate,
+			i;
+
+		end = this.options.display + this.options.offscreen - 1;
+		end = (end > this.numSlides) ? this.numSlides : end;
+		beg = this.numSlides - this.options.offscreen - 1;
+
+		// beginning
+		for (i = this.numSlides; i > beg; i--) {
+			duplicate = this.slides[i-1].cloneNode(true);						// cloneNode --> true is deep cloning
+			duplicate.removeAttribute('id');
+			duplicate.setAttribute('aria-hidden', 'true');
+			this._addClass( duplicate, 'clone');
+			this.slideWrap.insertBefore(duplicate, this.slideWrap.firstChild);	// add duplicate to beg'n of slides
+			this.cloned++;
+		}
+
+		// end
+		for (i = 0; i < end; i++) {
+			duplicate = this.slides[i].cloneNode(true);
+			duplicate.removeAttribute('id');
+			duplicate.setAttribute('aria-hidden', 'true');
+			this._addClass( duplicate, 'clone');
+			this.slideWrap.appendChild(duplicate);
+		}
+
+		// this.slideWrap.style.marginLeft = (-offscreen)+'00%';					// use marginLeft (not left) so IE8/9 etc can use left to slide
+	},
+
+	/**
+	 * Helper function to add a class to an element
+	 * @param  {int} i    Index of the slide to add a class to
+	 * @param  {string} name Class name
+	 * @return {void}
+	 */
+	_addClass: function(el, name) {
+		if (el.classList) { el.classList.add(name); }
+		else {el.className += ' ' + name; }
+	},
+
+	/**
+	 * Helper function to remove a class from an element
+	 * @param  {int} i    Index of the slide to remove class from
+	 * @param  {string} name Class name
+	 * @return {void}
+	 */
+	_removeClass: function(el, name) {
+		if (el.classList) { el.classList.remove(name); }
+		else { el.className = el.className.replace(new RegExp('(^|\\b)' + name.split(' ').join('|') + '(\\b|$)', 'gi'), ' '); }
+	},
+
+	/**
+	 * Helper function. Simple way to merge objects
+	 * @param  {object} obj A list of objects to extend
+	 * @return {object}     The extended object
+	 */
+	_extend: function(obj) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		for (var i = 0; i < args.length; i++) {
+			var source = args[i];
+			if (source) {
+				for (var prop in source) {
+					obj[prop] = source[prop];
+				}
 			}
+		}
+		return obj;
+	}
 
-			// if no method found and already init'd
-			if( $(this).data('init') ){
-				$.error( 'Method "' +  method + '" does not exist on ye olde carousel' );
-				return $(this);
-			}
+};
 
-			// otherwise, engage thrusters
-			if ( typeof method === 'object' || ! method ) {
-				$(this).data('init', true);
-
-				// return methods.init.apply( this, args );
-				return $.fn.carousel.prototype.init.apply( this, args );
-			}
-
-		});
-	};
-
-	// options  // TODO use these
-	$.fn.carousel.defaults = {
-		inClass: 'in',
-		outClass: 'out',
-		activeClass: 'active',
-		beforeClass: 'before',
-		afterClass: 'after',
-		// indicators: 'indicators',
-		// speed: 400,
-		slide: 'ul li',
-		infinite: true,
-		autoRotate: false,
-	};
-
-
-	// add methods
-	// Q.  Why? methods are already accessible from within this closure
-	// A.  if we want to extend the plugin with additional functionality, we need to make the element accessible
-	$.extend( $.fn.carousel.prototype, methods );
-
-}(jQuery));
