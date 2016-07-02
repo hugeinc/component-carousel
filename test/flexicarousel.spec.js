0ad03da8c558f38a7994d0d265d50d8165da0b6d
wes hatch <wes.hatch@gmail.com>
2016-07-02 19:38:10 -0400

    adding empty tests

---
+++ test/flexicarousel.spec.js
@@ -0,0 +1,44 @@
+describe('flexicarousel', function() {
+
+  it('should should export to the window object', function() {
+    // expect(...
+  });
+
+  it('should should export as a common module', function() {
+
+  });
+
+  it('initializes correctly', function() {
+
+  });
+
+  it('accepts the correct options', function() {
+    // range, mode, el
+  });
+
+  it('destroys cleanly, removing all references and events', function() {
+
+  });
+
+  it('goes to the next slide when next() is called', function() {
+    // expect(...
+  });
+
+  it('goes to the previous slide when prev() is called', function() {
+    // expect(...
+  });
+
+  it('jumps to slide 2 when go(2) is called', function() {
+    // expect(...
+  });
+
+  it('when we are at the last slide in an infinite carousel, goes to the first slide when next() is called', function() {
+    // expect(...
+  });
+
+  it('when we are at the first slide in an infinite carousel, goes to the last slide when prev() is called', function() {
+    // expect(...
+  });
+
+
+});
