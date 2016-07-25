3f6c78b9fdf815c9d9c830039fb4e56b3e399d1d
wes hatch <wes.hatch@gmail.com>
2016-07-24 23:54:39 -0400

    removing shim

--- src/shim.js
+++
@@ -1,6 +0,0 @@
-/**
- * Put Carousel into the Global scope.
- * Useful for existing demos or if you wish to include manually
- */
-import carousel from './flexicarousel.es6.js';
-window.Carousel = carousel;
