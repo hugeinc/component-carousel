ed9fb6f95047d4cc3c0f6f33020e49e80b1171b2
imagentleman <imagentlemail@gmail.com>
2017-10-07 22:52:21 -0500

    adding minified distribution file to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,27 @@
 import buble from 'rollup-plugin-buble';
+import uglify from "rollup-plugin-uglify";
 
-export default {
-  entry: 'src/carousel.js',
-  moduleName: 'Carousel',
-  plugins: [
-    buble()
-  ],
-  targets: [
-    { dest: 'dist/carousel.cjs.js', format: 'cjs' },
-    { dest: 'dist/carousel.es6.js', format: 'es' },
-    { dest: 'dist/carousel.js', format: 'iife' }
-  ]
-};
+export default [
+  {
+    entry: 'src/carousel.js',
+    moduleName: 'Carousel',
+    plugins: [
+      buble()
+    ],
+    targets: [
+      { dest: 'dist/carousel.cjs.js', format: 'cjs' },
+      { dest: 'dist/carousel.es6.js', format: 'es' },
+      { dest: 'dist/carousel.js', format: 'iife' }
+    ]
+  }, {
+    entry: 'src/carousel.js',
+    moduleName: 'Carousel',
+    plugins: [
+      buble(),
+      uglify()
+    ],
+    targets: [
+      { dest: 'dist/carousel.min.js', format: 'iife' }
+    ]
+  }
+];
