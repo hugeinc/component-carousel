b490f0b411eee9ac1bf6624b65144dbec80f6e7e
Wes Hatch <whatch@hugeinc.com>
2017-10-17 17:18:46 -0400

    fixing es6 bundling in prev build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
 import * as fs from 'fs';
 
 const license = fs.readFileSync('LICENSE', 'utf8');
@@ -10,25 +12,30 @@ export default {
     {
       file: 'dist/carousel.cjs.js',
       format: 'cjs',
-      banner: '/*\n' + license + '*/',
-      plugins: [
-        buble()
-      ]
+      banner: '/*!\n' + license + '*/'
     }, {
       file: 'dist/carousel.es6.js',
       format: 'es',
-      banner: '/*\n' + license + '*/',
-      plugins: [
-        buble()
-      ]
+      banner: '/*!\n' + license + '*/'
     }, {
       file: 'dist/carousel.js',
       format: 'iife',
       name: 'Carousel',
-      banner: '/*\n' + license + '*/',
-      plugins: [
-        buble()
-      ]
-    }
+      banner: '/*!\n' + license + '*/'
+    },
+  ],
+  plugins: [
+    buble(),
+    uglify({
+      output: {
+        comments: function(node, comment) {
+          var text = comment.value;
+          var type = comment.type;
+          if (type == "comment2") {
+            return /^!/i.test(text);
+          }
+        }
+      }
+    }, minify)
   ]
 };
